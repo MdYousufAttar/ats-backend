5fda21a4e4341786b4a7e10805219a2db8d6cc11
Mohammed Yousuf Attar <mohammedyousuf.attar@kyndryl.com>
2026-02-10 05:18:41 +0530

    Fix Gemini API URL

--- services/geminiService.js
+++ services/geminiService.js
@@ -25,9 +25,7 @@ Return STRICT JSON:
 `;
 
   try {
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent
-${process.env.API_KEY}`,
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.API_KEY}`,
       {
         method: "POST",
         headers: {
